90de60bb2f9f71b7f43dba816cbacec462967df5
Obaida Almelhem <111151192+Obaida00@users.noreply.github.com>
2022-09-13 12:02:00 +0300

    Update BasicCalculator.py

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -3,16 +3,17 @@ print("Hello and welcome to the basic calculater by OBAIDA ALMELHEM")
 num1 = float(input('Enter first number\n'))
 num2 = float(input('Enter second number\n'))
 
-oper = input("select an operator:\n [ x , / , + , - ]\n")
-print("Result: ", end='')
-if oper == 'x':
-    print(num1 * num2)
-elif oper == '/':
-    print(num1 / num2)
-elif oper == '+':
-    print(num1 + num2)
-elif oper == '-':
-    print(num1 - num2)
-else:
-    print('INVALID OPERATOR!!\n Please Try Again..')
-    # put all of it inside a function and call it from here
+def chose_oper()
+    oper = input("select an operator:\n [ x , / , + , - ]\n")
+    print("Result: ", end='')
+    if oper == 'x':
+        print(num1 * num2)
+    elif oper == '/':
+        print(num1 / num2)
+    elif oper == '+':
+        print(num1 + num2)
+    elif oper == '-':
+        print(num1 - num2)
+    else:
+        print('INVALID OPERATOR!!\n Please Try Again..')
+        chose_oper()
